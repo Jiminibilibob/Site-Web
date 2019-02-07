e7cdf8f75f019919e15ed883a7bfbb34c0bbbb0a
HAGER <hager@MacBook-Pro-de-HAGER.local>
2019-02-07 12:00:29 +0100

    update button

---
+++ source/scripts/my_script.js
@@ -0,0 +1,13 @@
+/*
+* init
+* called when body loaded
+*/
+function init() { 
+    document.getElementById("test").innerHTML = "init says hi hi hi low"
+}
+funtion test_button()  {
+    var my_test_div = document.getElementById("test");
+    my_test_div.innerHTML = "natachanatachapassonchatpachaquiséchapa";
+    my_test_div.style.backgroundColor = "green"
+alert("cliked");
+}
